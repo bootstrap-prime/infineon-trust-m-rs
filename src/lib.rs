8d9c0621bbd5c3bf14a820ceb391b4a010dbdac9
bootstrap-prime <bootstrap.prime@gmail.com>
2022-01-20 16:30:35 -0500

    make it so that extern function interfaces for the optiga PAL don't need to pass or grab anything from local contexts.

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,64 @@
+#![no_std]
+
 use embedded_hal::blocking::i2c::{Read, Write};
 use embedded_hal::digital::v2::OutputPin;
-use embedded_hal::timer::CountDown;
 
-use systick::{
-    delay::{delay_ms, delay_us},
-    micros, millis,
-};
+use systick::{delay::delay_ms, micros, millis};
 
-pub struct OptigaTrustM<RSTPin, VCCPin, I2CPin, TIMER>
+pub struct OptigaTrustM<RSTPin, VCCPin, I2CPin>
 where
     RSTPin: OutputPin,
     VCCPin: OutputPin,
     I2CPin: Write + Read,
-    TIMER: CountDown,
 {
     i2c: I2CPin,
     rst: RSTPin,
     pwr: VCCPin,
-    timer: TIMER,
 }
 
+pub trait OptigaResources {
+    fn set_rst_high(&mut self) -> bool;
+    fn set_rst_low(&mut self) -> bool;
+
+    fn set_pwr_high(&mut self) -> bool;
+    fn set_pwr_low(&mut self) -> bool;
+
+    fn read_i2c(&mut self, addr: u8, data: &mut [u8]) -> bool;
+    fn write_i2c(&mut self, addr: u8, data: &[u8]) -> bool;
+}
+
+#[allow(dead_code)]
 enum VDDorRST {
     PWR = 0,
     RST = 1,
+}
+
+impl<RSTPin, VCCPin, I2CPin> OptigaResources for OptigaTrustM<RSTPin, VCCPin, I2CPin>
+where
+    RSTPin: OutputPin,
+    VCCPin: OutputPin,
+    I2CPin: Write + Read,
+{
+    fn set_rst_high(&mut self) -> bool {
+        self.rst.set_high().is_ok()
+    }
+    fn set_rst_low(&mut self) -> bool {
+        self.rst.set_low().is_ok()
+    }
+
+    fn set_pwr_high(&mut self) -> bool {
+        self.pwr.set_high().is_ok()
+    }
+    fn set_pwr_low(&mut self) -> bool {
+        self.pwr.set_low().is_ok()
+    }
+
+    fn read_i2c(&mut self, addr: u8, data: &mut [u8]) -> bool {
+        self.i2c.read(addr, data).is_ok()
+    }
+    fn write_i2c(&mut self, addr: u8, data: &[u8]) -> bool {
+        self.i2c.write(addr, data).is_ok()
+    }
 }
 
 // from https://github.com/Infineon/optiga-trust-m/wiki/Porting-Guide
@@ -30,84 +66,104 @@ enum VDDorRST {
 // https://github.com/Infineon/arduino-optiga-trust-m/blob/master/src/optiga_trustm/pal_os_event_arduino.cpp
 // https://doc.rust-lang.org/book/ch19-01-unsafe-rust.html
 
-#[allow(no_mangle_generic_items)]
-impl<RSTPin, VCCPin, I2CPin, TIMER> OptigaTrustM<RSTPin, VCCPin, I2CPin, TIMER>
+unsafe impl<RSTPin, VCCPin, I2CPin> Send for OptigaTrustM<RSTPin, VCCPin, I2CPin>
 where
     RSTPin: OutputPin,
     VCCPin: OutputPin,
     I2CPin: Write + Read,
-    TIMER: CountDown,
 {
-    pub fn new(
-        rst: RSTPin,
-        pwr: VCCPin,
-        i2c: I2CPin,
-        timer: TIMER,
-    ) -> OptigaTrustM<RSTPin, VCCPin, I2CPin, TIMER> {
-        OptigaTrustM {
-            i2c,
-            rst,
-            pwr,
-            timer,
+}
+
+extern crate alloc;
+use alloc::boxed::Box;
+static mut OPTIGA_TRUST_M_RESOURCES: Option<Box<dyn OptigaResources + Send>> = None;
+
+#[allow(no_mangle_generic_items)]
+impl<RSTPin: 'static, VCCPin: 'static, I2CPin: 'static> OptigaTrustM<RSTPin, VCCPin, I2CPin>
+where
+    RSTPin: OutputPin,
+    VCCPin: OutputPin,
+    I2CPin: Write + Read,
+{
+    pub fn setup_new(rst: RSTPin, pwr: VCCPin, i2c: I2CPin) {
+        // user will need to configure the systick timer
+        let cursed_global_precursor = OptigaTrustM { i2c, rst, pwr };
+        unsafe {
+            OPTIGA_TRUST_M_RESOURCES = Some(Box::new(cursed_global_precursor));
         }
     }
+}
 
-    #[no_mangle]
-    pub extern "C" fn rust_hal_timer_delay_ms(&mut self, milliseconds: u16) {
-        delay_ms(milliseconds.into());
-    }
-
-    #[no_mangle]
-    pub extern "C" fn rust_hal_timer_get_time_ms(&mut self) -> u32 {
-        millis() as u32
-    }
-
-    #[no_mangle]
-    pub extern "C" fn rust_hal_timer_get_time_us(&mut self) -> u32 {
-        micros() as u32
-    }
-
-    #[no_mangle]
-    pub extern "C" fn rust_hal_gpio_set_high(&mut self, context: pal_gpio_t) -> bool {
-        match context.p_gpio_hw as u8 {
-            0 => self.pwr.set_high().is_ok(),
-            1 => self.rst.set_high().is_ok(),
-            _ => unreachable!(),
+#[no_mangle]
+pub extern "C" fn rust_hal_gpio_set_high(which_pin: u8) -> bool {
+    unsafe {
+        match &mut OPTIGA_TRUST_M_RESOURCES {
+            Some(periph) => match which_pin {
+                0 => periph.set_pwr_high(),
+                1 => periph.set_rst_high(),
+                _ => unreachable!(),
+            },
+            None => unreachable!(),
         }
     }
+}
 
-    #[no_mangle]
-    pub extern "C" fn rust_hal_gpio_set_low(&mut self, context: pal_gpio_t) -> bool {
-        match context.p_gpio_hw as u8 {
-            0 => self.pwr.set_low().is_ok(),
-            1 => self.rst.set_low().is_ok(),
-            _ => unreachable!(),
+#[no_mangle]
+pub extern "C" fn rust_hal_gpio_set_low(which_pin: u8) -> bool {
+    unsafe {
+        match &mut OPTIGA_TRUST_M_RESOURCES {
+            Some(periph) => match which_pin {
+                0 => periph.set_pwr_low(),
+                1 => periph.set_rst_low(),
+                _ => unreachable!(),
+            },
+            None => unreachable!(),
         }
     }
+}
 
-    #[no_mangle]
-    pub extern "C" fn rust_hal_i2c_read(
-        &mut self,
-        context: *const pal_i2c_t,
-        data: *mut u8,
-        len: u16,
-    ) -> bool {
-        let mut data = unsafe { core::slice::from_raw_parts(data, len.into()) };
-        let addr = context.slave_address as u8;
+#[no_mangle]
+pub extern "C" fn rust_hal_i2c_read(slave_address: u8, data: *mut u8, len: u16) -> bool {
+    let mut data = unsafe { core::slice::from_raw_parts_mut(data, len.into()) };
 
-        self.i2c.read(addr, &mut data).is_ok()
+    unsafe {
+        match &mut OPTIGA_TRUST_M_RESOURCES {
+            Some(periph) => periph.read_i2c(slave_address, &mut data),
+            None => unreachable!(),
+        }
     }
+}
 
-    #[no_mangle]
-    pub extern "C" fn rust_hal_i2c_write(
-        &mut self,
-        context: *const pal_i2c_t,
-        data: *mut u8,
-        len: u16,
-    ) -> bool {
-        let mut data = unsafe { core::slice::from_raw_parts(data, len.into()) };
-        let addr = context.slave_address as u8;
+#[no_mangle]
+pub extern "C" fn rust_hal_i2c_write(slave_address: u8, data: *const u8, len: u16) -> bool {
+    let data = unsafe { core::slice::from_raw_parts(data, len.into()) };
 
-        self.i2c.write(addr, &mut data).is_ok()
+    unsafe {
+        match &mut OPTIGA_TRUST_M_RESOURCES {
+            Some(periph) => periph.write_i2c(slave_address, data),
+            None => unreachable!(),
+        }
     }
+}
+
+#[no_mangle]
+pub extern "C" fn rust_hal_logger_log(log_data: *const u8, length: u32) {
+    let data = unsafe { core::slice::from_raw_parts(log_data, length as usize) };
+    let data = core::str::from_utf8(data).unwrap();
+    defmt::warn!("{}", data);
+}
+
+#[no_mangle]
+pub extern "C" fn rust_hal_timer_delay_ms(milliseconds: u16) {
+    delay_ms(milliseconds.into());
+}
+
+#[no_mangle]
+pub extern "C" fn rust_hal_timer_get_time_ms() -> u32 {
+    millis() as u32
+}
+
+#[no_mangle]
+pub extern "C" fn rust_hal_timer_get_time_us() -> u32 {
+    micros() as u32
 }
