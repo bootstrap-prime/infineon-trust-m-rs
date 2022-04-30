641a332935442764bb8b2bd540dcf471aec6fa81
bootstrap-prime <bootstrap.prime@gmail.com>
2022-04-30 12:22:33 -0400

    pal_i2c_context struct args were in the wrong order, and slave_address was in the place of the pointer to the upper layer context

--- optiga-m-sys/pal/pal_configures.c
+++ optiga-m-sys/pal/pal_configures.c
@@ -16,12 +16,12 @@ pal_i2c_t optiga_pal_i2c_context_0 =
 {
     /// Pointer to I2C master platform specific context
 	NULL,
-    /// Slave address
-    0x30,
     /// Upper layer context
     NULL,
     /// Callback event handler
-    NULL
+    NULL,
+    /// Slave address
+    0x30
 };
 
 pal_gpio_t optiga_vdd_0 =
