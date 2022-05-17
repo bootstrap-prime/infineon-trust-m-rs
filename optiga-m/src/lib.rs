d0f65587b476ab9ffae23224931975b6fe0e341d
bootstrap-prime <bootstrap.prime@gmail.com>
2022-05-17 15:56:12 -0400

    Partially implement DynDigest for hasher trait (currently failing)

--- optiga-m/src/lib.rs
+++ optiga-m/src/lib.rs
@@ -483,55 +483,14 @@ impl OptigaM {
 
 
 
-    pub fn sha256(&mut self, bits_to_hash: &[u8]) -> Result<[u8; 32], OptigaStatus> {
-        // initialize hash context
-        const CONTEXT_LENGTH: usize =
-            cbindings::optiga_hash_context_length_OPTIGA_HASH_CONTEXT_LENGTH_SHA_256 as usize;
-        let mut hash_context_buffer: [u8; CONTEXT_LENGTH] = [0; CONTEXT_LENGTH];
+    // pub fn sha256(&mut self, bits_to_hash: &[u8]) -> Result<[u8; 32], OptigaStatus> {
+    //     use core::ptr::{addr_of, addr_of_mut};
 
-        let mut hash_context: optiga_hash_context_t = {
-            optiga_hash_context {
-                context_buffer: hash_context_buffer.as_mut_ptr(),
-                context_buffer_length: hash_context_buffer.len() as u16,
-                hash_algo: optiga_hash_type_OPTIGA_HASH_TYPE_SHA_256 as u8,
-            }
-        };
+    //     #[cfg(not(test))]
+    //     defmt::trace!("starting hash");
 
-        let hash_data_host: hash_data_from_host_t = hash_data_from_host {
-            buffer: bits_to_hash.as_ptr(),
-            length: bits_to_hash.len() as u32,
-        };
-
-        use core::ptr::{addr_of, addr_of_mut};
-
-        #[cfg(not(test))]
-        defmt::trace!("starting hash");
-
-        call_optiga_func(|| unsafe {
-            optiga_crypt_hash_start(self.lib_crypt.as_ptr(), addr_of_mut!(hash_context))
-        })?;
-
-        call_optiga_func(|| unsafe {
-            optiga_crypt_hash_update(
-                self.lib_crypt.as_ptr(),
-                addr_of_mut!(hash_context),
-                OPTIGA_CRYPT_HOST_DATA as u8,
-                addr_of!(hash_data_host) as *const c_void,
-            )
-        })?;
-
-        let mut digest: [u8; 32] = [0; 32];
-
-        call_optiga_func(|| unsafe {
-            optiga_crypt_hash_finalize(
-                self.lib_crypt.as_ptr(),
-                addr_of_mut!(hash_context),
-                digest.as_mut_ptr(),
-            )
-        })?;
-
-        Ok(digest)
-    }
+    //     Ok(digest)
+    // }
 }
 
 impl From<OptigaStatus> for rand_core::Error {
@@ -553,6 +512,103 @@ impl From<rand_core::Error> for OptigaStatus {
         error.into()
     }
 }
+
+const OPTIGA_SHA256_CONTEXT_LENGTH: usize =
+    cbindings::optiga_hash_context_length_OPTIGA_HASH_CONTEXT_LENGTH_SHA_256 as usize;
+
+pub struct OptigaSha256<'a> {
+    periph: &'a mut OptigaM,
+    hash_context_buffer: [u8; OPTIGA_SHA256_CONTEXT_LENGTH],
+    hash_context: optiga_hash_context,
+}
+
+impl<'a> OptigaSha256<'a> {
+    pub fn new(periph: &'a mut OptigaM) -> Self {
+        // initialize hash context
+        let mut hash_context_buffer: [u8; OPTIGA_SHA256_CONTEXT_LENGTH] =
+            [0; OPTIGA_SHA256_CONTEXT_LENGTH];
+
+        let mut hash_context: optiga_hash_context_t = {
+            optiga_hash_context {
+                context_buffer: hash_context_buffer.as_mut_ptr(),
+                context_buffer_length: hash_context_buffer.len() as u16,
+                hash_algo: optiga_hash_type_OPTIGA_HASH_TYPE_SHA_256 as u8,
+            }
+        };
+
+        use core::ptr::{addr_of, addr_of_mut};
+
+        // start hashing operation
+        call_optiga_func(|| unsafe {
+            optiga_crypt_hash_start(periph.lib_crypt.as_ptr(), addr_of_mut!(hash_context))
+        })
+        .unwrap();
+
+        OptigaSha256 {
+            periph,
+            hash_context,
+            hash_context_buffer,
+        }
+    }
+}
+
+impl<'a> digest::DynDigest for OptigaSha256<'a> {
+    fn update(&mut self, data: &[u8]) {
+        use core::ptr::{addr_of, addr_of_mut};
+
+        let hash_data_host: hash_data_from_host_t = hash_data_from_host {
+            buffer: data.as_ptr(),
+            length: data.len() as u32,
+        };
+
+        call_optiga_func(|| unsafe {
+            optiga_crypt_hash_update(
+                self.periph.lib_crypt.as_ptr(),
+                addr_of_mut!(self.hash_context),
+                OPTIGA_CRYPT_HOST_DATA as u8,
+                addr_of!(hash_data_host) as *const c_void,
+            )
+        })
+        .unwrap();
+    }
+
+    fn finalize_into(mut self, digest: &mut [u8]) -> Result<(), digest::InvalidBufferSize> {
+        self.finalize_into_reset(digest)
+    }
+
+    fn finalize_into_reset(&mut self, digest: &mut [u8]) -> Result<(), digest::InvalidBufferSize> {
+        use core::ptr::addr_of_mut;
+
+        if digest.len() == 32 {
+            call_optiga_func(|| unsafe {
+                optiga_crypt_hash_finalize(
+                    self.periph.lib_crypt.as_ptr(),
+                    addr_of_mut!(self.hash_context),
+                    digest.as_mut_ptr(),
+                )
+            })
+            .unwrap();
+
+            Ok(())
+        } else {
+            Err(digest::InvalidBufferSize)
+        }
+    }
+
+    fn reset(&mut self) {
+        let mut digest: [u8; 32] = [0; 32];
+
+        self.finalize_into_reset(&mut digest).unwrap();
+    }
+
+    fn output_size(&self) -> usize {
+        32
+    }
+}
+
+impl<'a> digest::HashMarker for OptigaSha256<'a> {}
+
+pub use digest::DynDigest;
 
 /// Get random slice of bytes from the device's TRNG.
 /// The device's TRNG generates random numbers in blocks of 8 to 256 bytes - if an input buffer exceeds this, the hardware is called multiple times to fill it.
