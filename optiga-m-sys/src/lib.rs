3c69c62a0ab6ded1638c8e1e95c35dde5318442d
bootstrap-prime <bootstrap.prime@gmail.com>
2022-05-19 12:39:18 -0400

    rename broken_intra_doc_links to rustdoc::broken_intra_doc_links

--- optiga-m-sys/src/lib.rs
+++ optiga-m-sys/src/lib.rs
@@ -12,7 +12,7 @@ lazy_static::lazy_static! {
 pub mod cbindings {
     #![allow(dead_code)]
     #![allow(non_camel_case_types)]
-    #![allow(broken_intra_doc_links)]
+    #![allow(rustdoc::broken_intra_doc_links)]
     #![allow(non_upper_case_globals)]
     #![allow(non_snake_case)]
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
